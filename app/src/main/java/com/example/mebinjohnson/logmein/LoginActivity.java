c2e1f4863800ae459bdfcc00db823bac81f82c6e
mebinjohnson <mebinjohnson@gmail.com>
2018-07-03 23:17:09 +0400

    Firebase Integration 65%

--- app/src/main/java/com/example/mebinjohnson/logmein/LoginActivity.java
+++ app/src/main/java/com/example/mebinjohnson/logmein/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.mebinjohnson.logmein;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private ProgressBar progressBar;
+
     private EditText mEmailField;
     private EditText mPasswordField;
     private TextView mCreateAccount;
@@ -40,17 +44,38 @@ public class LoginActivity extends AppCompatActivity {
 
 
 // Set the dimensions of the sign-in button.
-        SignInButton msignInButton = findViewById(R.id.sign_in_button);
-        msignInButton.setSize(SignInButton.SIZE_STANDARD);
+//        SignInButton msignInButton = findViewById(R.id.sign_in_button);
+//        msignInButton.setSize(SignInButton.SIZE_STANDARD);
 
         initViews();
 
         mAuth = FirebaseAuth.getInstance();
 
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user!=null){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
         mCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar = new ProgressBar(LoginActivity.this, null, android.R.attr.progressBarStyleLarge);
+
+                String email = mEmailField.getText().toString().trim();
+                String password = mPasswordField.getText().toString().trim();
+
+                signIn(email,password);
             }
         });
     }
