0985b165bfadfbc45dbbcf7ef48e7d07db83d973
mebinjohnson <mebinjohnson@gmail.com>
2018-07-03 22:39:53 +0400

    Registration Activity Created and Firebase Integration 60%

--- app/src/main/java/com/example/mebinjohnson/logmein/LoginActivity.java
+++ app/src/main/java/com/example/mebinjohnson/logmein/LoginActivity.java
@@ -1,28 +1,141 @@
 package com.example.mebinjohnson.logmein;
 
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+
+    private FirebaseAuth mAuth;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private TextView mCreateAccount;
+    private Button mLogInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        //Remove title bar
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//
-////Remove notification bar
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){}
-
-//set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_login);
+
+
+// Set the dimensions of the sign-in button.
+        SignInButton msignInButton = findViewById(R.id.sign_in_button);
+        msignInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        initViews();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+
+    private void initViews() {
+
+        mEmailField = findViewById(R.id.field_email);
+        mPasswordField = findViewById(R.id.field_password);
+
+        mCreateAccount = findViewById(R.id.tvCreateAccount);
+        mLogInButton = findViewById(R.id.log_in_button);
+
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //TODO: showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //TODO: updateUI(user);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //TODO: updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            //mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+
 }
+
+
---
+++ app/src/main/java/com/example/mebinjohnson/logmein/RegisterActivity.java
@@ -0,0 +1,120 @@
+package com.example.mebinjohnson.logmein;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    private EditText mUserName, mUserPassword, mUserMail, mUserAge;
+    private TextView mSignIn;
+
+    private Button mRegButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        initViews();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validateForm()){
+                    //Uploading Data to Firebase
+
+                    String email = mUserMail.getText().toString().trim();
+                    String password = mUserPassword.getText().toString().trim();
+
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Account Created!", Toast.LENGTH_SHORT).show();
+
+//                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+//                                        .setDisplayName("Jane Q. User")
+//                                        .build();
+
+                                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Account Creation Failed!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+
+
+    private void initViews() {
+        mUserName = findViewById(R.id.field_reg_name);
+        mUserPassword = findViewById(R.id.field_reg_password);
+        mUserMail = findViewById(R.id.field_reg_mail);
+        mUserAge = findViewById(R.id.field_reg_age);
+        mRegButton = findViewById(R.id.create_account_button);
+        mSignIn = findViewById(R.id.tvSignIn);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mUserMail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mUserMail.setError("Required.");
+            valid = false;
+        } else {
+            mUserMail.setError(null);
+        }
+
+        String password = mUserPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mUserPassword.setError("Required.");
+            valid = false;
+        } else {
+            mUserPassword.setError(null);
+        }
+        String agestr = mUserAge.getText().toString();
+        int age = Integer.parseInt( mUserAge.getText().toString());
+        if (TextUtils.isEmpty(agestr)) {
+            mUserAge.setError("Required.");
+            valid = false;
+        } else if (age < 15) {
+            mUserAge.setError("You're to Young");
+            valid = false;
+        } else {
+            mUserAge.setError(null);
+        }
+        return valid;
+    }
+}
